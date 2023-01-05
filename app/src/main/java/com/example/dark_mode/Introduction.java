dde4e9c7692a2fb0b137176e4e184ae396a5ef85
Pavan Parmar <pavan@gmail.com>
2023-01-05 15:46:39 +0530

    darkmode

--- app/src/main/java/com/example/dark_mode/Introduction.java
+++ app/src/main/java/com/example/dark_mode/Introduction.java
@@ -7,19 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Purchase extends AppCompatActivity {
+public class Introduction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchase);
+        setContentView(R.layout.activity_introduction);
+        AppCompatButton button=findViewById(R.id.intro_btn);
 
-        AppCompatButton button=findViewById(R.id.purchase_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    startActivity(new Intent(getApplicationContext(),HomeScreen.class));
+                startActivity(new Intent(getApplicationContext(),SignIn.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/dark_mode/SignIn.java
+++ app/src/main/java/com/example/dark_mode/SignIn.java
@@ -1,8 +1,12 @@
 package com.example.dark_mode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        AppCompatButton button = findViewById(R.id.signin_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Purchase.class));
+            }
+        });
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        isDarkTheme();
+//    }
+//
+//    private boolean isDarkTheme() {
+//        return (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+//    }
 }
